c3a26192c2fb009cb53401594b3fbd38cdd5f7ff
Vitaliy Zhidkikh <vitalyzhidkih@gmail.com>
2020-12-21 22:36:57 +0300

    restructured app for react

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            This is the root container for other components
+        </div>
+    )
+}
+
+export default App;
