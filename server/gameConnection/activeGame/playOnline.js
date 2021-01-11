00ab1d7fb8e989fb33100903bbed43e1352ff120
Vitaliy Zhidkikh <vitalyzhidkih@gmail.com>
2021-01-11 23:14:14 +0300

    added simple auth and refactor

--- server/gameConnection/activeGame/playOnline.js
+++ server/gameConnection/activeGame/playOnline.js
@@ -1,5 +1,6 @@
-//everything related to game logic itself goes in here
-
 module.exports = (sockets, roomId) => {
+    
+    //everything related to game logic itself goes in here
+    
     console.log('');
 }
