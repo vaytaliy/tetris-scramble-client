1a763bd697f765e38fd015eaac41a3de6e4a3382
vaytaliy <53967995+vaytaliy@users.noreply.github.com>
2021-01-16 18:51:11 +0300

    Update proxy.js

--- client/src/configuration/proxy.js
+++ client/src/configuration/proxy.js
@@ -1,5 +1,5 @@
 //Address parameters, change if needed
 
-const rootAddress = 'https://tetriscramble.herokuapp.com/'
+const rootAddress = 'https://tetriscramble.herokuapp.com'
 
 export default rootAddress;
